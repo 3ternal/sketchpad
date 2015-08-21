fd9b282d4684d0fc39fb6ab95418a73f52918afa
eternal <tainted.eternity01@gmail.com>
2015-08-20 17:24:17 -0700

    finished

---
+++ sketchpad.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  for (var row = 1; row <= 16; row++)
+  {
+    $(".wrapper").append("<tr>");
+  }
+  for (var column = 1; column <= 16; column++)
+  {
+    $("tr").append("<td><div class=square></div></td>");
+  }
+
+  $("tr").wrapAll('<table></table>');
+
+  $(".wrapper").on("mouseover", ".square", function()
+  {
+    console.log("hii");
+    $(this).css("background-color", "blue")
+  });
+
+  $("#new_grid").click(function() {
+    var newGrid = Number(prompt("How many rows?"));
+
+    if (isNaN(newGrid))
+    {
+      console.log(newGrid);
+      alert("Please enter a number.");
+    }
+
+    if (!isNaN(newGrid))
+    {
+      $("table").remove();
+
+      for (var row = 1; row <= newGrid; row++)
+      {
+        $(".wrapper").append("<tr>");
+      }
+      for (var column = 1; column <= newGrid; column++)
+      {
+        $("tr").append("<td><div class=square></div></td>");
+      }
+
+      $("tr").wrapAll('<table></table>');
+    }
+  });
+
+});
